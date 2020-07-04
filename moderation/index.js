ab48b387ed5c4fc184d74f4f4b6ced560e360985
shimin <thunderous_0518@hotmail.com>
2020-07-04 16:59:10 -0400

    blog sample with microservice

---
+++ moderation/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/events", async (req, res) => {
+  console.log("Recevied Events", req.body.type);
+  const { type, data } = req.body;
+
+  if (type === "CommentCreated") {
+    console.log("CommentCreated");
+    const status = data.content.includes("orange") ? "rejected" : "approved";
+
+    await axios.post("http://eventbus-srv:4005/events", {
+      type: "CommentModerated",
+      data: {
+        id: data.id,
+        content: data.content,
+        postId: data.postId,
+        status,
+      },
+    });
+  }
+
+  res.send({});
+});
+
+app.listen(4003, () => {
+  console.log("Listening on 4003");
+});
